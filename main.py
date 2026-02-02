237408278cb9c2e6995498d01b1c02fbd62213a0
Igor Molchanov <141867556+TsipaPte@users.noreply.github.com>
2026-02-03 00:58:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"Главный файл - точка входа в приложение"
+
+import functions
+
+def main() -> None:
+    """Функция запускающая главную логику"""
+    
+    functions.run()
+
+if __name__ == "__main__":
+    main()
