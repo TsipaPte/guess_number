d84ead6ea5104eac359bcdedc694488c4ed9b992
Igor Molchanov <141867556+TsipaPte@users.noreply.github.com>
2026-02-04 00:28:16 +0500

    Integrate click to pause before exiting

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 "Главный файл - точка входа в приложение"
 
+import click
 import functions
 
 def main() -> None:
     """Функция запускающая главную логику"""
     
     functions.run()
+
+    click.pause("Нажмите любую клавишу для выхода ...")
 
 if __name__ == "__main__":
     main()
